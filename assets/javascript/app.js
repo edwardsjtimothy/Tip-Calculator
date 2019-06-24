2ac31d236a6296bb91abca16327d28a1a519c2a7
Enragedtoast <edwardsjtimothy@gmail.com>
2019-06-24 12:08:38 -0500

    modified html to include input fields to make focusing on them easier

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,14 @@
 $(function() {
 
+   
     $(".input-display").click(function() {
         $(".input-display").css("border","solid 1px black");
         $(this).css("border", "3px solid rgb(241, 185, 29)");
+
     });
     
-    $(".num-btn").click(function () {
-        if ($(".input-display").css("border", "3px solid rgb(241, 185, 29)")) {
-            $("#total").text($(this).data("num"));
-
-        }
+    $(".num-btn").click(function() {
+       
     });
 
 
