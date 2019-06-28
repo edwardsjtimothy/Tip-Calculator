e8977a78677cc2d38aac8ca71fc3538377c23420
Enragedtoast <edwardsjtimothy@gmail.com>
2019-06-28 16:56:11 -0500

    added input validation and popover

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,27 @@
-$(function() {
+$(function () {
 
-
-//variables and click functions used to determine which input is focused for the purpose of using HTML number pad
+    //variables and click functions used to determine which input is focused for the purpose of using HTML number pad
 
     var totalFocused = true;
     var percFocused = false;
-    var splitFocused = false; 
+    var splitFocused = false;
     var myNum = "";
     var floatingPoint = false;
     var decimalCount = 3;
 
-    $("#total-dis").click(function() {
+    $("#total-dis").click(function () {
         totalFocused = true;
         percFocused = false;
         splitFocused = false;
     });
 
-    $("#perc-dis").click(function() {
+    $("#perc-dis").click(function () {
         totalFocused = false;
         percFocused = true;
         splitFocused = false;
     });
 
-    $("#split-dis").click(function() {
+    $("#split-dis").click(function () {
         totalFocused = false;
         percFocused = false;
         splitFocused = true;
@@ -33,42 +32,42 @@ $(function() {
     $("#total-dis").css("border", "3px solid rgb(240, 173, 78)");
 
     //focus on clicked input element and display 
-   
-    $(".input-display").click(function() {
-        $(".input-display").css("border","solid 1px black");
+
+    $(".input-display").click(function () {
+        $(".input-display").css("border", "solid 1px black");
         $(this).css("border", "3px solid rgb(240, 173, 78)");
         $(this).find("input").focus();
     });
 
     //number buttons writing to focused input fields
 
-    $(".num-btn").click(function() {
+    $(".num-btn").click(function () {
         var selectedButton = $(this).html();
-        var selectedVal =  parseFloat(selectedButton);
-        
+        var selectedVal = parseFloat(selectedButton);
+
         if (totalFocused === true) {
-            $("#total").val(function(index, val) {
-                
+            $("#total").val(function (index, val) {
+
                 //input fields with type="number" do not like decimals when using html key pad. This is a work around. 
 
-                if(selectedButton === "." && floatingPoint === false){ 
+                if (selectedButton === "." && floatingPoint === false) {
                     myNum = myNum + ".";
-                    floatingPoint = true; 
+                    floatingPoint = true;
                 } else {
-                    if(floatingPoint) decimalCount--;
-                    if(decimalCount > 0) myNum = myNum+""+selectedVal
+                    if (floatingPoint) decimalCount--;
+                    if (decimalCount > 0) myNum = myNum + "" + selectedVal
                 };
 
-                if(selectedButton !== ".") return parseFloat(myNum)
+                if (selectedButton !== ".") return parseFloat(myNum)
                 else return (parseFloat(myNum).toFixed(2));
             });
-            
+
         } else if (percFocused === true) {
-            $("#perc").val(function(index, val) {
+            $("#perc").val(function (index, val) {
                 return val + selectedButton;
             });
         } else if (splitFocused === true) {
-            $("#split").val(function(index, val) {
+            $("#split").val(function (index, val) {
                 return val + selectedButton;
             });
         };
@@ -76,7 +75,7 @@ $(function() {
 
     //backspace 
 
-    $(".back-btn").click(function() {
+    $(".back-btn").click(function () {
         if (totalFocused === true) {
             var value = $("#total").val();
             value = value.substr(0, value.length - 1);
@@ -95,47 +94,82 @@ $(function() {
 
     //reset 
 
-    $(".clear-btn").click(function() {
+    $(".clear-btn").click(function () {
         $("input").val("");
         $("#split").val(1);
-        $(".input-display").css("border","solid 1px black");
+        $(".input-display").css("border", "solid 1px black");
         $("#total-dis").css("border", "3px solid rgb(240, 173, 78)");
         $("#total").focus();
         $("#tip-tot").html("");
 
         totalFocused = true;
-        percFocused = false; 
-        splitFocused = false; 
+        percFocused = false;
+        splitFocused = false;
         floatingPoint = false;
         decimalCount = 3;
         myNum = "";
     });
+
+
 
 
     //calculate on click of calculate button
 
-    $(".cal-btn").click(function() {
+    $(".cal-btn").click(function () {
+
+         //enabling popovers for error checking messages
+
+        $('[data-toggle="popover"]').popover();
+
+        //variables for the math
+
         var total = $("#total").val().trim();
-        var perc = $("#perc").val().trim()/ 100;
+        var perc = $("#perc").val().trim() / 100;
         var split = $("#split").val().trim();
         var result = total * perc / split;
-        $("#tip-tot").html(parseFloat(result).toFixed(2));
-        floatingPoint = false;
-        decimalCount = 3;
-        myNum = "";
-        // console.log(floatingPoint)
+
+        //checking for empty strings
+
+        if (split === "" || split === 0) {
+            split = 1;
+        };
+
+        if (perc === "") {
+            result = total / split;
+        };
+
+        //checking for invalid input and throwing error popover if found
+
+        if (parseFloat(total) === 0 || Math.sign(parseFloat(total)) === -1 || total === "") {
+            $(".cal-btn").attr("data-content", "Total can't be 0, a negative number, or be left blank.")
+            $(".cal-btn").popover("show");
+            $("#total").val("");
+        } else if (parseFloat(perc) === 0 || Math.sign(parseFloat(perc)) === -1) {
+            $(".cal-btn").attr("data-content", "The tip percentage can't be 0 or a negative number.")
+            $(".cal-btn").popover("show");
+            $("#perc").val("");
+        } else {
+            
+            $("#tip-tot").html(parseFloat(result).toFixed(2));
+
+            //reseting html keypad variables 
+            floatingPoint = false;
+            decimalCount = 3;
+            myNum = "";
+
+        };
     });
 
     //limits input of total to two digits after decimal 
 
-    $(function() {
-        $("#total").keyup(function(){
-          if($(this).val().indexOf(".")!=-1){         
-              if($(this).val().split(".")[1].length > 2) {                
-                  if( isNaN( parseFloat(this.value))) return;
-                  this.value = parseFloat(this.value).toFixed(2);
-              }  
-           }            
+    $(function () {
+        $("#total").keyup(function () {
+            if ($(this).val().indexOf(".") != -1) {
+                if ($(this).val().split(".")[1].length > 2) {
+                    if (isNaN(parseFloat(this.value))) return;
+                    this.value = parseFloat(this.value).toFixed(2);
+                }
+            }
         });
-       });
+    });
 });
